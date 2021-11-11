29b29becdf198c8e98b193a0e119df595c856dfd
trung1999hy <trantrung1999hy@gmail.com>
2021-11-11 16:02:57 +0700

    trung

---
+++ app/src/main/java/com/tvt/doc_bao_online/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tvt.doc_bao_online
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
